368dd92b65402f6005151935c2b7617a56c35fd8
Kevin Adams <adamsk@msoe.edu>
2021-10-17 13:00:02 -0500

    skeleton file

---
+++ as3RecDec.py
@@ -0,0 +1,42 @@
+def main():
+    complexInput = input().split()
+    parse_Plans()
+
+#Returns the next token
+def next_token():
+    return None
+
+#Advances token forward
+def advance():
+    return None
+
+def parse_Plans():
+    return None
+
+def parse_Complex():
+    return None
+
+def parse_Building():
+    return None
+
+def parse_FloorList():
+    return None
+
+def parse_FloorReference():
+    return None
+
+def parse_Floor():
+    return None
+
+def parse_RoomList():
+    return None
+
+def parse_Room():
+    return None
+
+def parse_Name():
+    return None
+
+def parse_Number():
+    return None
+
