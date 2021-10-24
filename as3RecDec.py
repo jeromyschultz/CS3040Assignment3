be8a0ff38d3c7246d622f4851595cc49d4da49b5
Kevin Adams <adamsk@msoe.edu>
2021-10-24 15:36:39 -0500

    Got parsing working for Test1Basic.txt. Still needs testing for incorrect input

--- as3RecDec.py
+++ as3RecDec.py
@@ -1,25 +1,35 @@
+import sys
+import re
+
 token = 0
-print("Enter a string")
-complexInput = input().split()
+lineNum = 1
+data = sys.stdin.read()
+complexInput = data.split()
 complexlen = len(complexInput)
+name = re.compile("[A-Z_]+")
 
 def main():
     parse_Plans()
 
-
 #Returns the next token
 def next_token():
-    return complexInput[token]
+    if complexInput:
+        return complexInput[token]
 
 #Advances token forward
 def advance():
     if complexInput:
         complexInput.pop(0)
 
+def advanceEnd():
+    print(complexInput)
+    complexInput.clear()
+
 #Returns true if expected matches next_token and prints an error if false
 def expected(value):
-    if next_token != value:
-        print("\'" + value + "\' expected")
+    if next_token() != value:
+        print("\'" + value + "\' expected on line " + str(lineNum))
+        advanceEnd()
         return False
     else:
         return True
@@ -39,18 +49,22 @@ def parse_Complex():
 
 def parse_Building():
     if expected('building'):
+        advance()
         parse_Name()
     if expected('with'):
         advance()
-    if expected('floors') or expected('floor'):
+    if next_token() == 'floor' or next_token() == 'floors':
         advance()
         parse_FloorList()
+    else:
+        print("\'floor(s)\' expected on line " + str(lineNum))
 
 def parse_FloorList():
     if expected('{'):
         advance()
         parse_FloorReference()
     while next_token() == ',':
+        advance()
         parse_FloorReference()
     if expected('}'):
         advance()
@@ -64,11 +78,12 @@ def parse_Floor():
         parse_Name()
     if expected('has'):
         advance()
-    if next_token() != 'room' or next_token != 'rooms':
-        print("\'room(s)\' expected")
-    else:
+    if next_token() == 'room' or next_token() == 'rooms':
         advance()
         parse_RoomList()
+    else:
+        print("\'room(s)\' expected on line " + str(lineNum))
+        advanceEnd()
 
 def parse_RoomList():
     if expected('['):
@@ -87,16 +102,19 @@ def parse_Room():
         parse_Number()
 
 def parse_Name():
-    if not next_token().isalpha():
+    if not name.fullmatch(next_token()):
         print("Name cannot contain non alphebetical numbers")
+        advanceEnd()
     elif not next_token().isupper():
         print("Name is expected to be upper case")
+        advanceEnd
     else:
         advance()
 
 def parse_Number():
     if not next_token().isnumeric:
         print("Number Expected")
+        advanceEnd()
     else:
         advance()
 
